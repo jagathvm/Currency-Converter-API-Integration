f1424238fae0754f8f02f01ea88740af7576cb7b
JagathVM <17jagathvm@gmail.com>
2025-01-22 23:52:40 +0530

    Refactor: Set up GET route for fetching currency rates with getRates controller

--- src/routes/rates.js
+++ src/routes/rates.js
@@ -1,8 +1,11 @@
+// Imports
 import { Router } from "express";
 import { getRates } from "../controllers/rates-controller.js";
 
+// Routes
 const router = Router();
 
 router.get("/rates", getRates);
 
+// Export
 export default router;
